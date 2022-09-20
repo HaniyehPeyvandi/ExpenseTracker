63d18998ef71799500361df64d1fc43b0256f5a8
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-20 18:19:57 +0430

    add state of formValues to TransactionForm

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,17 +1,55 @@
+import { useState } from "react";
+
 const TransactionForm = () => {
-  return ( 
+  const [formValues, setFormValues] = useState({
+    amount: 0,
+    desc: "",
+    type: "expense",
+  });
+
+  const changeHandler = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  return (
     <form>
-      <input type="number" placeholder="Amount"  name="amount"/>
-      <input type="text" placeholder="Description" name="desc"/>
+      <input
+        type="number"
+        placeholder="Amount"
+        name="amount"
+        onChange={changeHandler}
+        value={formValues.amount}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        name="desc"
+        onChange={changeHandler}
+        value={formValues.desc}
+      />
       <div>
-        <input type="radio" name="type" id="expense"/>
+        <input
+          type="radio"
+          name="type"
+          id="expense"
+          value="expense"
+          onChange={changeHandler}
+          checked={formValues.type === "expense"}
+        />
         <label htmlFor="expense">Expense</label>
-        <input type="radio" name="type" id="income"/>
+        <input
+          type="radio"
+          name="type"
+          id="income"
+          value="income"
+          onChange={changeHandler}
+          checked={formValues.type === "income"}
+        />
         <label htmlFor="income">Income</label>
       </div>
       <button type="submit">Add Transaction</button>
     </form>
-   );
-}
- 
+  );
+};
+
 export default TransactionForm;
