7502f32221ada9d94b0aa75321a0ee2a21ef9aa1
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-20 12:44:11 +0430

    add functionality of Add button in Overview component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,18 +1,28 @@
-import styles from './Overview.module.css';
+import { useState } from "react";
+import styles from "./Overview.module.css";
 
-const Overview = ({expense,income}) => {
-  return ( 
+const Overview = ({ expense, income }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  return (
     <>
       <div className={styles.topSection}>
         <p>Balance: {income - expense}</p>
-        <button>Add</button>
+        <button
+          onClick={() => {
+            setIsShow((prevState) => !prevState);
+          }}
+        >
+          {isShow ? "Cancel" : "Add"}
+        </button>
       </div>
+      {isShow && <div>Form</div>}
       <div className={styles.resultSection}>
         <div>Expense {expense}</div>
         <div>Income {income}</div>
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default Overview;
