22c024797c749285974f55308bda9b9fd5b0c653
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-20 20:55:43 +0430

    add code for rendering transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,7 +1,20 @@
-const Transaction = (props) => {
+const Transaction = ({ transactions }) => {
   return (
-    <section>Transactions</section>
-   );
-}
- 
+    <section>
+      {transactions.length ? (
+        transactions.map((t) => {
+          return (
+            <div key={t.id}>
+              <span>{t.desc}/</span>
+              <span>{t.amount}</span>
+            </div>
+          );
+        })
+      ) : (
+        <p>Add some transactions!</p>
+      )}
+    </section>
+  );
+};
+
 export default Transaction;
