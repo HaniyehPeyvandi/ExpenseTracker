39548a21db8f8d3d3e971cb35dc2c116fb4b2a7b
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-20 21:26:20 +0430

    add code for updating income and expense

--- src/components/ExpenseApp.js
+++ src/components/ExpenseApp.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./ExpenseApp.module.css";
 import Overview from "./Overview";
 import Transaction from "./Transaction";
@@ -7,6 +7,18 @@ const ExpenseApp = () => {
   const [expense, setExpense] = useState(0);
   const [income, setIncome] = useState(0);
   const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    let exp = 0;
+    let inc = 0;
+    transactions.forEach((t) => {
+      t.type === "expense"
+        ? (exp += parseFloat(t.amount))
+        : (inc += parseFloat(t.amount));
+    });
+    setExpense(exp);
+    setIncome(inc);
+  }, [transactions]);
 
   const addTransaction = (formValues) => {
     setTransactions([...transactions, { ...formValues, id: Date.now() }]);
