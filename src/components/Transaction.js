56e1b218f1089c49edb2cc77f2128ba3dfd3d546
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-23 23:50:32 +0330

    add code for searching transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import styles from "./Transaction.module.css";
 
-const Transaction = ({ transactions }) => {
+const Transaction = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredTnx, setFilteredTnx] = useState(props.transactions);
+
+  useEffect(() => {
+    filterTransactions(searchValue);
+  }, [props.transactions]);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilteredTnx(props.transactions);
+      return;
+    }
+    const filtered = props.transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTnx(filtered);
+  };
+
+  const searchHandler = (e) => {
+    setSearchValue(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
   return (
     <section>
-      {transactions.length ? (
-        transactions.map((t) => {
+      <h3 className={styles.transactionHeading}>Transactions</h3>
+      <input
+        type="text"
+        onChange={searchHandler}
+        value={searchValue}
+        className={styles.search}
+        placeholder="Search"
+      />
+      {!props.transactions.length ? (
+        <p>Add some transactions!</p>
+      ) : filteredTnx.length ? (
+        filteredTnx.map((t) => {
           return (
             <div
               key={t.id}
@@ -17,7 +51,7 @@ const Transaction = ({ transactions }) => {
           );
         })
       ) : (
-        <p>Add some transactions!</p>
+        <p>No item matches!</p>
       )}
     </section>
   );
